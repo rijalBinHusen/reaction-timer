2447d12803bfd0e639330ec3bcc792d80b0d4c2c
rijal <jeryzal333@gmail.com>
2022-06-02 05:02:51 +0700

    Block components

---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="block">
+        Click me
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Block",
+    props: ["delay"]
+}
+</script>
+
+<style scoped>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
